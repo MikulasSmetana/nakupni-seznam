cf743ebe20b32498bc3b964bbcd1329b761d3bc2
smetana mikulas <smetana.mikulas>
2024-09-23 10:37:46 +0200

    Přidaná fce odebrat_polozku

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ def pridat_polozku(seznam):
     polozka = input("Zadejte název položky k přidání: ")
     seznam.append(polozka)
     print(f"Položka {polozka} byla přidána.")
+
+def odebrat_polozku(seznam):
+    polozka = input("Zadejte název položky k odebrání: ")
+    seznam.remove(polozka)
+    print(f"Položka {polozka} byla odebrána: ")
+
