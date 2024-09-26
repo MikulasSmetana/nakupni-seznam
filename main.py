541d398f83b12a7939592456f8677e4a0d81fd0d
MikulasSmetana <118127593+MikulasSmetana@users.noreply.github.com>
2024-09-26 16:28:34 +0200

    Definované fce

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Definované Fce
+seznam = []
 def pridat_polozku(seznam):
     polozka = input("Zadejte název položky k přidání: ")
     seznam.append(polozka)
@@ -7,4 +9,18 @@ def odebrat_polozku(seznam):
     polozka = input("Zadejte název položky k odebrání: ")
     seznam.remove(polozka)
     print(f"Položka {polozka} byla odebrána: ")
+    
+def zobrazit_seznam():
+    print(f"Seznam obsahuje tyto položky: {seznam}")
 
+def seradit_seznam():
+    seznam_sorted = sorted(seznam)
+    print("Seznam byl úspěšně seřazen podle abecedy.")
+    print(seznam_sorted)
+
+def pocet_polozek():
+    size = len(seznam)  
+    print(f"Nyní se v seznamu nachází {size} položky.")
+
+def ukoncit_program():
+    exit()
